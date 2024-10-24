4785be0cefb3ec38d441d258e9c4d171e3f60a35
Kevin Nørby Andersen <kevin@superultra.dk>
2024-10-24 21:26:12 +0200

    add transparent background and change brush to smaller circle

--- src/components/PaintEditor.vue
+++ src/components/PaintEditor.vue
@@ -12,20 +12,33 @@ new p5(sketch => {
 
   sketch.setup = () => {
     sketch.createCanvas(512, 512).parent('c')
-    sketch.background(255)
     sketch.image(img, 0, 0)
-    sketch.fill(100)
-    sketch.rect(50, 50, 50, 50)
+
+    let saveButton = document.getElementById('save')
+    saveButton.addEventListener('click', () => {
+      sketch.save('myCanvas.png')
+    })
   }
 
   // Use mouse events or other logic to erase part of the image
   sketch.mouseDragged = () => {
     sketch.erase()
-    sketch.rect(sketch.mouseX, sketch.mouseY, 50, 50) // Erase by drawing a rectangle
+    sketch.circle(sketch.mouseX, sketch.mouseY, 20) // Erase by drawing a circle
     sketch.noErase()
   }
 })
 </script>
 <template>
   <div id="c" />
+  <br />
+  <button id="save">Save</button>
 </template>
+<style scoped>
+#c {
+  width: 512px;
+  height: 512px;
+  display: inline-block;
+  background-image: url('data:image/svg+xml,<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(%23clip0_700_190)"><rect width="40" height="40" fill="white"/><rect width="20" height="20" fill="%23D9D9D9"/><rect x="20" y="20" width="20" height="20" fill="%23D9D9D9"/></g><defs><clipPath id="clip0_700_190"><rect width="40" height="40" fill="white"/></clipPath></defs></svg>');
+  background-repeat: repeat;
+}
+</style>
